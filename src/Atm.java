4971c80dccaba03d8004c04798a039303365c2f3
feel-eel <43239998+feel-eel@users.noreply.github.com>
2018-11-18 17:14:08 -0500

    finished

---
+++ src/Atm.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class Atm {
+	private BankAccount bankAccount;
+	
+	public Atm(BankAccount bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+	
+	public BankAccount getBankAccount() {
+		return bankAccount;
+	}
+
+	public void setBankAccount(BankAccount bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+	
+	public void Menu() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter pin:");
+		int pin = in.nextInt();
+		if (pin == bankAccount.getUser().getPIN()) {
+			System.out.println("Enter account number:");
+			long num = in.nextLong();
+			if (num == bankAccount.getAccountNumber()) {
+			
+				int option = 0;
+				while (option != 4) {
+					System.out.println("Press 1 to deposit, 2 to withdraw, 3 show balance, 4 to exit");
+					 option = in.nextInt();
+					 
+				if (option != 1 && option != 2 && option != 3 && option != 4) {
+					System.out.println("Enter amount greater than 0");
+				} 
+				
+				else if (option == 1) {
+					System.out.println("How much would you like to deposit?");
+					int money = in.nextInt();
+					int returnV = bankAccount.deposit(money);
+						if (returnV == 1) {
+							System.out.println("Now there is " + bankAccount.getBalance() + " dollars in your account");
+						} else {
+							System.out.println("Enter amount greater than 0");
+						}
+				} 
+				
+				else if (option == 2) {
+					if (bankAccount.getBalance() == 0) {
+						System.out.println("There is no money to take out :(");
+					} else {
+						System.out.println("How much would you like to withdraw?");
+						int money = in.nextInt();
+						int returnV = bankAccount.withdraw(money);
+						if (returnV == 2) {
+							System.out.println("Now there is " + bankAccount.getBalance() + " dollars in your account");
+						} 
+						else if (returnV == 1){
+							System.out.println("Enter amount greater than 0");
+						} else {
+							System.out.println("Sorry, you're too broke for that :(");
+						}				
+					} 
+				}
+				
+				else if (option == 3) {
+					System.out.println("There is " + bankAccount.getBalance() + " dollars in your account");	
+				} 
+				
+				else if (option == 4) {
+					System.out.println("Have a good day!");
+				} 
+			}
+			}
+			else {
+				System.out.println("Credentials invalid");
+			}
+		}
+		else {
+			System.out.println("Credentials invalid");
+		}
+		in.close();
+	}
+		
+	
+	
+	public static void main(String[] args) {
+	
+		Atm atm = new Atm(new BankAccount(0, new User(1111, "Julia", "2/11/01", "3 Cherry Road")));
+		
+		atm.Menu();
+	}
+}
+
